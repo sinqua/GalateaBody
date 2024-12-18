fe8e8daa4d5d891f98ed7acd1d387fabb0de291b
규화 신 <quashine@icloud.com>
2024-12-18 21:43:01 +0900

    using meta all in one package

--- Assets/Network.cs
+++ Assets/Network.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -10,23 +8,9 @@ public class Network : MonoBehaviour
     public TMP_InputField inputField;
     public TextToSpeech textToSpeech;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     public void OnSendButtonClicked()
     {
-        // StartCoroutine(GetDataFromServer());
-        //StartCoroutine(SendDataToServer(inputField.text));
         StartCoroutine(SendMessageToServer(inputField.text));
-    }
-    [System.Serializable]
-    public class Message
-    {
-        public string message;
     }
 
     IEnumerator SendMessageToServer(string text)
@@ -48,52 +32,5 @@ public class Network : MonoBehaviour
             Debug.Log(request.downloadHandler.text);
             textToSpeech.ButtonClick(request.downloadHandler.text);
         }
-    }
-
-
-    IEnumerator SendDataToServer(string text)
-    {
-        string url = "http://localhost:8080/voice";
-        Message message = new Message{ message = text };
-        string jsonData = JsonUtility.ToJson(message);
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
-        UnityWebRequest request = new UnityWebRequest(url, "POST");
-        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            Debug.Log("Data sent successfully");
-        }
-    }
-
-    IEnumerator GetDataFromServer()
-    {
-        string url = "http://localhost:8000";
-        UnityWebRequest request = UnityWebRequest.Get(url);
-
-        yield return request.SendWebRequest();
-
-        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
-        {
-            Debug.Log(request.error);
-        }
-        else
-        {
-            Debug.Log(request.downloadHandler.text);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
